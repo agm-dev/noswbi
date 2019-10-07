56a5f1b430b748c7ba811b3aa202e0fa52f00526
Adrián Gonzalo <gonzahh@gmail.com>
2019-10-07 07:12:45 +0200

    added tests for router

--- tests/domain/router.spec.js
+++ tests/domain/router.spec.js
@@ -1,14 +1,43 @@
+const express = require("express");
+const passport = require("passport");
 const { createRouter } = require("../../src/domain/router");
 
 const router = createRouter();
 
 describe("createRouter", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   const requiredMethods = ["get", "post", "put", "patch", "delete"];
 
-  requiredMethods.forEach(method => {
-    test(`should return a router that has "${method}" method`, () => {
-      expect(router).toHaveProperty(method);
-      expect(typeof router[method]).toBe("function");
+  describe(`has ${requiredMethods.join(", ")} methods`, () => {
+    requiredMethods.forEach(method => {
+      test(`should return a router that has "${method}" method`, () => {
+        expect(router).toHaveProperty(method);
+        expect(typeof router[method]).toBe("function");
+      });
     });
+  });
+
+  test("uses auth when enabled on config", () => {
+    const spyAuth = jest.spyOn(passport, "authenticate");
+    const spyRouter = jest.spyOn(express, "Router").mockReturnValue({
+      use: () => {}
+    });
+    createRouter({ requireAuth: true });
+    expect(spyRouter).toHaveBeenCalledTimes(1);
+    expect(spyAuth).toHaveBeenCalledTimes(1);
+    expect(spyAuth).toHaveBeenCalledWith("jwt", { session: false });
+  });
+
+  test("doesn't use auth when not enabled on config", () => {
+    const spyAuth = jest.spyOn(passport, "authenticate");
+    const spyRouter = jest.spyOn(express, "Router").mockReturnValue({
+      use: () => {}
+    });
+    createRouter({ requireAuth: false });
+    expect(spyRouter).toHaveBeenCalledTimes(1);
+    expect(spyAuth).toHaveBeenCalledTimes(0);
   });
 });
